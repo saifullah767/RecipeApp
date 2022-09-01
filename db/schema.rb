797abc3928c6c50d5b427c97bfaa43ba9956b60b
saifullah767 <skj99667788@gmail.com>
2022-09-01 10:44:19 +0500

    add ref to food

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_053858) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_054124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_053858) do
     t.integer "quantity", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
@@ -45,4 +47,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_053858) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "users"
 end
