b58270bc0c0b5a5e3f903a7bb3701b0acc29d10e
saifullah767 <skj99667788@gmail.com>
2022-09-01 16:54:05 +0500

    seeds

--- db/migrate/20220901111006_create_foods.rb
+++ db/migrate/20220901111006_create_foods.rb
@@ -1,7 +1,10 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
-
+      t.string :name
+      t.string :measurement_unit, default: 'g'
+      t.float :price, default: 0.0
+      t.integer :quantity, default: 0
       t.timestamps
     end
   end
