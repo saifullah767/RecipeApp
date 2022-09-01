e84498740c7d023f38b6151769d181f121215f9a
saifullah767 <skj99667788@gmail.com>
2022-09-02 01:37:13 +0500

    rubocop error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,13 @@
 class Ability
   include CanCan::Ability
 
-    def initialize(user)
-      can :read, Recipe, public: true
-  
-      return unless user.present?
-  
-      can :manage, Recipe, user_id: user.id
-      can :manage, Food, user_id: user.id
+  def initialize(user)
+    can :read, Recipe, public: true
+
+    return unless user.present?
+
+    can :manage, Recipe, user_id: user.id
+    can :manage, Food, user_id: user.id
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
