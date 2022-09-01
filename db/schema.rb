0c2f8762954a7b5674679711e54a8d3f1bf8efac
saifullah767 <skj99667788@gmail.com>
2022-09-01 10:38:13 +0500

    recipes migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_053522) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_053713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_053522) do
     t.string "measurement_unit", default: "g"
     t.float "price", default: 0.0
     t.integer "quantity", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.float "preparation_time", default: 0.0
+    t.float "cooking_time", default: 0.0
+    t.text "description", default: ""
+    t.boolean "public", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
